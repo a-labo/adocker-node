d0344ae43e75c4d02c33a4f9a1dc9162ec1d1a75
Taka Okunishi <okunishinishi@gmail.com>
2016-11-14 21:38:53 +0900

    Initial commit

---
+++ test/adocker_node_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for adockerNode.
+ * Runs with mocha.
+ */
+'use strict'
+
+const adockerNode = require('../lib/adocker_node.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('adocker-node', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Adocker node', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
