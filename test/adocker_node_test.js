83b23c757c215f876a3327a66fb592b78db6af61
okunishinishi <okunishinishi@gmail.com>
2019-02-24 09:32:18 +0900

    Version incremented to 2.0.6

--- test/adocker_node_test.js
+++ test/adocker_node_test.js
@@ -7,40 +7,30 @@
 const adockerNode = require('../lib/adocker_node.js')
 const { ok, equal } = require('assert')
 const asleep = require('asleep')
-const co = require('co')
 
 describe('adocker-node', function () {
   this.timeout(300000)
 
-  before(() => co(function * () {
-
-  }))
-
-  after(() => co(function * () {
-
-  }))
-
-  it('Adocker node', () => co(function * () {
+  it('Adocker node', async () => {
     let { logs, purge, build, run, remove, hasContainer, isRunning, exec } = adockerNode('testing-adocker-node', {
-      image: 'node:6',
+      image: 'node:8',
       tag: 'adocker-node-test',
       workdir: `${__dirname}/../misc/mocks/mock-project-01`,
-      cmd: [ 'node', './bin/app.js' ]
+      cmd: ['node', './bin/app.js']
     }).cli()
     ok(logs)
     ok(run)
 
-    yield build()
-    yield run()
+    await build()
+    await run()
 
-    // ok(yield hasContainer())
-    // ok(yield isRunning())
+    // ok(await hasContainer())
+    // ok(await isRunning())
 
+    await asleep(1000)
 
-    yield asleep(1000)
-
-    yield remove({ force: true })
-  }))
+    await remove({ force: true })
+  })
 })
 
 /* global describe, before, after, it */
